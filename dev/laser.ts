fca47b5ede21731e9155534c8cfcf7aa7aeef438
Imani <maniflames@users.noreply.github.com>
2017-06-21 10:34:03 +0200

    Update laser.ts

--- dev/laser.ts
+++ dev/laser.ts
@@ -10,9 +10,6 @@ class Laser extends GameObject{
 
     move() : void {
         super.move();
-        if(this.object.position.z > 10){
-            this.game.scene.remove(this.object);
-        }
         if(this.object.position.z < -80){
             this.game.scene.remove(this.object); 
         }
